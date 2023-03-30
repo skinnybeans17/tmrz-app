52d35d47ef15e7ba8d84f48b9c85788e63a9deb8
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2023-03-30 14:12:53 -0700

    timers running

--- src/components/TimerView.js
+++ src/components/TimerView.js
@@ -4,10 +4,26 @@ import { toggleTimer } from "../features/timers/timersSlice";
 
 export default function TimerView({ index, name, time, isRunning }) {
 	const dispatch = useDispatch()
+
+	const formatTime = (time) => {
+		let hours = Math.round(time / 1000 / 60 / 60)
+		let minutes = Math.round(time / 1000 / 60 % 60)
+		let seconds = Math.round(time / 1000 % 60)
+		if (seconds < 10) {
+			seconds = `0${seconds}`
+		}
+		if (minutes < 10) {
+			minutes = `0${minutes}`
+		}
+		if (hours < 10) {
+			hours = `0${hours}`
+		}
+		return `${hours}:${minutes}:${seconds}`
+	}
 	return (
 		<div>
 			<h2>{name}</h2>
-			<h1>{time}</h1>
+			<h1>{formatTime(time)}</h1>
 			<button
 				onClick={() => dispatch(toggleTimer(index))}
 			>
